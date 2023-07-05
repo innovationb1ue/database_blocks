e5cbc5929b0fa18d7a87a68719f8982e314c8bf6
jeffzhong <517262600@qq.com>
2023-07-05 22:31:40 +0800

    update

--- libs/spdlog-1.11.0/include/spdlog/details/thread_pool-inl.h
+++ libs/spdlog-1.11.0/include/spdlog/details/thread_pool-inl.h
@@ -54,8 +54,8 @@ namespace spdlog {
             SPDLOG_CATCH_STD
         }
 
-        void SPDLOG_INLINEthread_pool::post_log(async_logger_ptr &&worker_ptr, const details::log_msg &msg,
-                                                async_overflow_policy overflow_policy) {
+        void SPDLOG_INLINE thread_pool::post_log(async_logger_ptr &&worker_ptr, const details::log_msg &msg,
+                                                 async_overflow_policy overflow_policy) {
             async_msg async_m(std::move(worker_ptr), async_msg_type::log, msg);
             post_async_msg_(std::move(async_m), overflow_policy);
         }
